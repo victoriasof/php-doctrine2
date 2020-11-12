ff52467ba7c1c20cdbe113d9805caaa181839518
Victoria <victoria.sofianidou@gmail.com>
2020-11-12 15:47:33 +0100

    added constructor and two methods, RestStudent and RestTeacher

--- src/Controller/RestController.php
+++ src/Controller/RestController.php
@@ -2,19 +2,48 @@
 
 namespace App\Controller;
 
+use App\Repository\TeacherRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RestController extends AbstractController
 {
+
+    private TeacherRepository $teacherRepository;
+
     /**
-     * @Route("/rest", name="rest")
+     * RestController constructor.
+     * @param TeacherRepository $teacherRepository
      */
-    public function index(): Response
+    public function __construct(TeacherRepository $teacherRepository)
+    {
+        $this->teacherRepository = $teacherRepository;
+    }
+
+    //dependency injection ?
+
+
+    /**
+     * @Route("/rest-student", name="restStudent")
+     */
+    public function RestStudent(): Response
     {
         return $this->render('rest/index.html.twig', [
             'controller_name' => 'RestController',
         ]);
     }
+
+
+    /**
+     * @Route("/rest-teacher", name="restTeacher")
+     */
+    public function RestTeacher(): Response
+    {
+        return $this->render('rest/index.html.twig', [
+            'controller_name' => 'RestController',
+        ]);
+    }
+
+
 }
